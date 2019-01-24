3364bfda0fdb4ce43fdd943a041a39a2c4478817
Jeffrey Chang <me@jeffchang.io>
2019-01-24 09:50:14 -0800

    Converted to AndroidX

--- app/src/main/java/io/jeffchang/stackoverflowlist/ui/MainActivity.java
+++ app/src/main/java/io/jeffchang/stackoverflowlist/ui/MainActivity.java
@@ -1,7 +1,7 @@
 package io.jeffchang.stackoverflowlist.ui;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 
 import dagger.android.support.DaggerAppCompatActivity;
 import io.jeffchang.stackoverflowlist.R;
--- app/src/main/java/io/jeffchang/stackoverflowlist/ui/stackoverflowlist/StackOverFlowUserListFragment.java
+++ app/src/main/java/io/jeffchang/stackoverflowlist/ui/stackoverflowlist/StackOverFlowUserListFragment.java
@@ -1,8 +1,8 @@
 package io.jeffchang.stackoverflowlist.ui.stackoverflowlist;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import android.view.View;
 
 import io.jeffchang.stackoverflowlist.R;
